e0c1fd9a38578def73a628b4acc367f7a950a5f1
LimpSword <contact@limpsword.fr>
2024-01-08 17:21:49 +0100

    Easier equals between models

--- src/main/java/fr/_14/pcd/codingweek15/model/User.java
+++ src/main/java/fr/_14/pcd/codingweek15/model/User.java
@@ -6,6 +6,7 @@ import lombok.Setter;
 
 import javax.persistence.*;
 import java.util.List;
+import java.util.Objects;
 
 @Entity
 @Setter
@@ -41,4 +42,16 @@ public final class User {
         this.admin = admin;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
