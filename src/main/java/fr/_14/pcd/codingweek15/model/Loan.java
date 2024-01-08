ced3670963600fe272e08ce48b10d1e641478f07
LimpSword <contact@limpsword.fr>
2024-01-08 20:17:21 +0100

    Removed debug prints

--- src/main/java/fr/_14/pcd/codingweek15/model/Loan.java
+++ src/main/java/fr/_14/pcd/codingweek15/model/Loan.java
@@ -52,8 +52,6 @@ public final class Loan {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Loan loan = (Loan) o;
-        System.out.println("id = " + id + ", startDate = " + startDate + ", endDate = " + endDate + ", item = " + item + ", borrower = " + borrower + ", rating = " + rating);
-        System.out.println("loan.id = " + loan.id + ", loan.startDate = " + loan.startDate + ", loan.endDate = " + loan.endDate + ", loan.item = " + loan.item + ", loan.borrower = " + loan.borrower + ", loan.rating = " + loan.rating);
         return Objects.equals(id, loan.id) && Objects.equals(startDate, loan.startDate) && Objects.equals(endDate, loan.endDate) && Objects.equals(item, loan.item) && Objects.equals(borrower, loan.borrower) && Objects.equals(rating, loan.rating);
     }
 
