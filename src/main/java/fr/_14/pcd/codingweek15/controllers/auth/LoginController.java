2a350e23e928a75d804d3bfa8fce477c5578208c
Lucas Laurent <114945778+kour0@users.noreply.github.com>
2024-01-09 16:25:11 +0100

    FIX: Add redirections

--- src/main/java/fr/_14/pcd/codingweek15/controllers/auth/LoginController.java
+++ src/main/java/fr/_14/pcd/codingweek15/controllers/auth/LoginController.java
@@ -23,7 +23,7 @@ public class LoginController {
         }
 
         if (AuthService.getInstance().authenticate(enteredEmail, enteredPassword)) {
-            LayoutManager.setLayout("auth/register.fxml", "login success");
+            LayoutManager.setLayout("borrow/index.fxml", "login success");
             return;
         }
 
