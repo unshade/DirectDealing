6139f49cca1c1c41a9d7ea3507579fb9881c1dcb
Lucas Laurent <114945778+kour0@users.noreply.github.com>
2024-01-09 10:05:00 +0100

    FEATURE: Users Test

--- src/main/java/fr/_14/pcd/codingweek15/database/seeder/UserSeeder.java
+++ src/main/java/fr/_14/pcd/codingweek15/database/seeder/UserSeeder.java
@@ -1,6 +1,7 @@
 package fr._14.pcd.codingweek15.database.seeder;
 
 import fr._14.pcd.codingweek15.dao.UserDAO;
+import fr._14.pcd.codingweek15.service.AuthService;
 
 public class UserSeeder {
 
@@ -8,10 +9,12 @@ public class UserSeeder {
 
         UserDAO userDAO = UserDAO.getInstance();
 
+        String pass = AuthService.getInstance().getEncoder().encode("password");
+
         // Create users
-        userDAO.createUser("John", "Doe", "john.doe@telecomnancy.eu", "password", 0, false, true);
-        userDAO.createUser("Jane", "Doe", "john.doe@telecomnancy.eu", "password", 0, false, false);
-        userDAO.createUser("Michele", "Doe", "michele.doe@telecomnancy.eu", "password", 0, false, false);
+        userDAO.createUser("John", "Doe", "john.doe@telecomnancy.eu", pass, 0, false, true);
+        userDAO.createUser("Jane", "Doe", "john.doe@telecomnancy.eu", pass, 0, false, false);
+        userDAO.createUser("Michele", "Doe", "michele.doe@telecomnancy.eu", pass, 0, false, false);
 
     }
 }
