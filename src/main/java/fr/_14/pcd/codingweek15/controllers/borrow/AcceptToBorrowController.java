0d5b6cbe1eb3a40cf5e97e37b055bb2f0444ba72
unshade <72690981+unshade@users.noreply.github.com>
2024-01-09 17:00:47 +0100

    kouro

--- src/main/java/fr/_14/pcd/codingweek15/controllers/borrow/AcceptToBorrowController.java
+++ src/main/java/fr/_14/pcd/codingweek15/controllers/borrow/AcceptToBorrowController.java
@@ -1,4 +1,10 @@
 package fr._14.pcd.codingweek15.controllers.borrow;
 
+import javafx.fxml.FXML;
+
 public class AcceptToBorrowController {
+    @FXML
+    private void accept() {
+        // TODO
+    }
 }
