9394288377a030dc54101d678aeec233ab9fee32
unshade <72690981+unshade@users.noreply.github.com>
2024-01-11 17:01:34 +0100

    FEATURE : restyle borrow

--- src/main/java/fr/quatorze/pcd/codingweekquinze/controllers/borrow/SelectElementController.java
+++ src/main/java/fr/quatorze/pcd/codingweekquinze/controllers/borrow/SelectElementController.java
@@ -3,38 +3,46 @@ package fr.quatorze.pcd.codingweekquinze.controllers.borrow;
 import fr.quatorze.pcd.codingweekquinze.dao.ElementDAO;
 import fr.quatorze.pcd.codingweekquinze.layout.LayoutManager;
 import fr.quatorze.pcd.codingweekquinze.layout.RequiresAuth;
+import fr.quatorze.pcd.codingweekquinze.layout.component.AutocompletionTextField;
 import fr.quatorze.pcd.codingweekquinze.model.Element;
 import fr.quatorze.pcd.codingweekquinze.service.AuthService;
+import io.github.palexdev.materialfx.controls.*;
+import io.github.palexdev.materialfx.controls.cell.MFXTableRowCell;
+import io.github.palexdev.materialfx.filter.IntegerFilter;
+import io.github.palexdev.materialfx.filter.StringFilter;
+import io.github.palexdev.materialfx.utils.others.observables.When;
 import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
+import javafx.geometry.Pos;
 import javafx.scene.control.*;
 import javafx.util.Callback;
 
+import java.net.URL;
 import java.time.ZoneOffset;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
 @RequiresAuth
 public class SelectElementController {
 
     @FXML
-    private ListView<Element> elements;
+    private MFXTableView<Element> elements;
 
     @FXML
-    private TextField searchBar;
+    private MFXTextField searchBar;
 
     @FXML
-    private DatePicker startDate;
+    private MFXDatePicker startDate;
 
     @FXML
-    private DatePicker endDate;
+    private MFXDatePicker endDate;
 
     @FXML
-    private ComboBox<String> rating;
+    private MFXComboBox<String> rating;
 
     @FXML
-    private ComboBox<String> type;
+    private MFXComboBox<String> type;
+    @FXML
+    private AutocompletionTextField cityBar;
 
 
     @FXML
@@ -49,34 +57,58 @@ public class SelectElementController {
         this.rating.setValue("Sélectionnez une note");
         this.type.setValue("Sélectionnez un type");
 
-        List<Element> elements = ElementDAO.getInstance().getAllElementExceptUser(AuthService.getInstance().getCurrentUser());
 
-        this.elements.setItems(FXCollections.observableList(elements));
-        this.elements.setCellFactory(new Callback<ListView<Element>, ListCell<Element>>() {
-            @Override
-            public ListCell<Element> call(ListView<Element> listView) {
-                return new ListCell<Element>() {
-                    @Override
-                    protected void updateItem(Element element, boolean empty) {
-                        super.updateItem(element, empty);
-                        if (empty || element == null) {
-                            setText(null);
-                            setOnMouseClicked(null);
-                        } else {
-                            String content = "ID: " + element.getId() + ", Title: " + element.getName() +
-                                    ", Price: " + element.getPrice() + ", Description: " + element.getDescription();
-                            setText(content);
-                            setOnMouseClicked(event -> {
-                                if (event.getClickCount() == 2 && (!isEmpty())) {
-                                    LayoutManager.setLayout("borrow/create_loan_view.fxml", "Loan", element);
-                                }
-                            });
+        setupTable();
 
-                        }
-                    }
-                };
-            }
-        });
+        elements.autosizeColumnsOnInitialization();
+
+//        this.elements.setCellFactory(new Callback<ListView<Element>, ListCell<Element>>() {
+//            @Override
+//            public ListCell<Element> call(ListView<Element> listView) {
+//                return new ListCell<Element>() {
+//                    @Override
+//                    protected void updateItem(Element element, boolean empty) {
+//                        super.updateItem(element, empty);
+//                        if (empty || element == null) {
+//                            setText(null);
+//                            setOnMouseClicked(null);
+//                        } else {
+//                            String content = "ID: " + element.getId() + ", Title: " + element.getName() +
+//                                    ", Price: " + element.getPrice() + ", Description: " + element.getDescription();
+//                            setText(content);
+//                            setOnMouseClicked(event -> {
+//                                if (event.getClickCount() == 2 && (!isEmpty())) {
+//                                    LayoutManager.setLayout("borrow/create_loan_view.fxml", "Loan", element);
+//                                }
+//                            });
+//
+//                        }
+//                    }
+//                };
+//            }
+//        });
+    }
+
+    private void setupTable() {
+        MFXTableColumn<Element> nameColumn = new MFXTableColumn<>("Titre", true, Comparator.comparing(Element::getName));
+        MFXTableColumn<Element> surnameColumn = new MFXTableColumn<>("Prix", true, Comparator.comparing(Element::getPrice));
+        MFXTableColumn<Element> ageColumn = new MFXTableColumn<>("Description", true, Comparator.comparing(Element::getDescription));
+
+        nameColumn.setRowCellFactory(person -> new MFXTableRowCell<>(Element::getName));
+        surnameColumn.setRowCellFactory(person -> new MFXTableRowCell<>(Element::getPrice));
+        ageColumn.setRowCellFactory(person -> new MFXTableRowCell<>(Element::getDescription) {{
+            setAlignment(Pos.CENTER_RIGHT);
+        }});
+        ageColumn.setAlignment(Pos.CENTER_RIGHT);
+
+        elements.getTableColumns().addAll(nameColumn, surnameColumn, ageColumn);
+//        elements.getFilters().addAll(
+//                new StringFilter<>("Name", Element::getName),
+//                new StringFilter<>("Surname", Element::getPrice),
+//                new IntegerFilter<>("Age", Element::getDescription)
+//        );
+        List<Element> elems = ElementDAO.getInstance().getAllElementExceptUser(AuthService.getInstance().getCurrentUser());
+        this.elements.setItems(FXCollections.observableList(elems));
     }
 
     private void search() {
