10eb211514ebcf1cf2000528de7c6959a2f49797
unshade <72690981+unshade@users.noreply.github.com>
2024-01-11 14:40:59 +0100

    FIX : Configure MFX

--- src/main/java/fr/quatorze/pcd/codingweekquinze/layout/LayoutManager.java
+++ src/main/java/fr/quatorze/pcd/codingweekquinze/layout/LayoutManager.java
@@ -4,6 +4,9 @@ import fr.quatorze.pcd.codingweekquinze.MainApplication;
 import fr.quatorze.pcd.codingweekquinze.controllers.NavBarController;
 import fr.quatorze.pcd.codingweekquinze.service.AuthService;
 import fr.quatorze.pcd.codingweekquinze.util.FXMLLoaderUtil;
+import io.github.palexdev.materialfx.theming.MaterialFXStylesheets;
+import io.github.palexdev.materialfx.theming.UserAgentBuilder;
+import io.github.palexdev.materialfx.theming.JavaFXThemes;
 import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -11,6 +14,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+
 
 import java.io.IOException;
 
@@ -25,6 +29,15 @@ public final class LayoutManager {
         if (LayoutManager.stage != null) {
             throw new IllegalStateException("LayoutManager already initialized");
         }
+
+
+        UserAgentBuilder.builder()
+                .themes(JavaFXThemes.MODENA) // Optional if you don't need JavaFX's default theme, still recommended though
+                .themes(MaterialFXStylesheets.forAssemble(true)) // Adds the MaterialFX's default theme. The boolean argument is to include legacy controls
+                .setDeploy(true) // Whether to deploy each theme's assets on a temporary dir on the disk
+                .setResolveAssets(true) // Whether to try resolving @import statements and resources urls
+                .build() // Assembles all the added themes into a single CSSFragment (very powerful class check its documentation)
+                .setGlobal(); // Finally, sets the produced stylesheet as the global User-Agent stylesheet
 
         LayoutManager.stage = stage;
         currentLayout = "main-layout.fxml";
