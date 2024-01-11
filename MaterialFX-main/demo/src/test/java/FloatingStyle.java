ff43aee9776ebc834be93bb58a6b0bd1476f9155
unshade <72690981+unshade@users.noreply.github.com>
2024-01-11 14:49:57 +0100

    FIX : Configure MFX

--- MaterialFX-main/demo/src/test/java/FloatingStyle.java
+++
@@ -1,3 +0,0 @@
-public enum FloatingStyle {
-	FLOATING, IFTA
-}
--- MaterialFX-main/demo/src/test/java/Launcher.java
+++
@@ -1,8 +0,0 @@
-import javafx.application.Application;
-
-public class Launcher {
-
-	public static void main(String[] args) {
-		Application.launch(Reproducer.class, args);
-	}
-}
