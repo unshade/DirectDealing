b20fe159493b6216c8069922d7be97cea342d9d3
Alexis MARCEL <AlexisMarcel55@gmail.com>
2024-01-12 14:42:07 +0100

    Fix seeder

--- src/main/java/fr/quatorze/pcd/codingweekquinze/database/seeder/AvailabilitySeeder.java
+++ src/main/java/fr/quatorze/pcd/codingweekquinze/database/seeder/AvailabilitySeeder.java
@@ -17,21 +17,21 @@ public class AvailabilitySeeder {
         AvailabilityDAO availabilityDAO = AvailabilityDAO.getInstance();
 
         // 15 objects
-        availabilityDAO.createAvailability(elements.get(0), java.time.LocalDateTime.of(2024, 12, 12, 12, 12), java.time.LocalDateTime.of(2024, 12, 12, 12, 12), ChronoUnit.DAYS, 0);
-        availabilityDAO.createAvailability(elements.get(1), java.time.LocalDateTime.of(2024, 1, 11, 12, 12), java.time.LocalDateTime.of(2024, 1, 11, 12, 12), ChronoUnit.DAYS, 0);
-        availabilityDAO.createAvailability(elements.get(2), java.time.LocalDateTime.of(2024, 2, 10, 12, 12), java.time.LocalDateTime.of(2024, 2, 10, 12, 12), ChronoUnit.DAYS, 0);
-        availabilityDAO.createAvailability(elements.get(3), java.time.LocalDateTime.of(2024, 3, 9, 12, 12), java.time.LocalDateTime.of(2024, 4, 9, 12, 12), ChronoUnit.DAYS, 0);
-        availabilityDAO.createAvailability(elements.get(4), java.time.LocalDateTime.of(2024, 4, 8, 12, 12), java.time.LocalDateTime.of(2024, 5, 8, 12, 12), ChronoUnit.DAYS, 0);
-        availabilityDAO.createAvailability(elements.get(5), java.time.LocalDateTime.of(2024, 5, 7, 12, 12), java.time.LocalDateTime.of(2024, 6, 7, 12, 12), ChronoUnit.DAYS, 0);
-        availabilityDAO.createAvailability(elements.get(6), java.time.LocalDateTime.of(2024, 6, 6, 12, 12), java.time.LocalDateTime.of(2024, 7, 6, 12, 12), ChronoUnit.DAYS, 0);
+        availabilityDAO.createAvailability(elements.get(0), java.time.LocalDateTime.of(2024, 11, 12, 12, 12), java.time.LocalDateTime.of(2024, 12, 12, 12, 12), ChronoUnit.DAYS, 0);
+        availabilityDAO.createAvailability(elements.get(1), java.time.LocalDateTime.of(2024, 1, 11, 12, 12), java.time.LocalDateTime.of(2024, 2, 11, 12, 12), ChronoUnit.DAYS, 0);
+        availabilityDAO.createAvailability(elements.get(2), java.time.LocalDateTime.of(2024, 1, 10, 12, 12), java.time.LocalDateTime.of(2024, 2, 10, 12, 12), ChronoUnit.DAYS, 0);
+        availabilityDAO.createAvailability(elements.get(3), java.time.LocalDateTime.of(2024, 2, 9, 12, 12), java.time.LocalDateTime.of(2024, 4, 9, 12, 12), ChronoUnit.DAYS, 0);
+        availabilityDAO.createAvailability(elements.get(4), java.time.LocalDateTime.of(2024, 3, 8, 12, 12), java.time.LocalDateTime.of(2024, 5, 8, 12, 12), ChronoUnit.DAYS, 0);
+        availabilityDAO.createAvailability(elements.get(5), java.time.LocalDateTime.of(2024, 4, 7, 12, 12), java.time.LocalDateTime.of(2024, 6, 7, 12, 12), ChronoUnit.DAYS, 0);
+        availabilityDAO.createAvailability(elements.get(6), java.time.LocalDateTime.of(2024, 5, 6, 12, 12), java.time.LocalDateTime.of(2024, 7, 6, 12, 12), ChronoUnit.DAYS, 0);
         availabilityDAO.createAvailability(elements.get(7), java.time.LocalDateTime.of(2024, 7, 5, 12, 12), java.time.LocalDateTime.of(2024, 8, 5, 12, 12), ChronoUnit.DAYS, 0);
         availabilityDAO.createAvailability(elements.get(8), java.time.LocalDateTime.of(2024, 8, 4, 12, 12), java.time.LocalDateTime.of(2024, 9, 4, 12, 12), ChronoUnit.DAYS, 0);
         availabilityDAO.createAvailability(elements.get(9), java.time.LocalDateTime.of(2024, 9, 3, 12, 12), java.time.LocalDateTime.of(2024, 10, 3, 12, 12), ChronoUnit.DAYS, 0);
         availabilityDAO.createAvailability(elements.get(10), java.time.LocalDateTime.of(2024, 10, 2, 12, 12), java.time.LocalDateTime.of(2024, 11, 2, 12, 12), ChronoUnit.DAYS, 0);
         availabilityDAO.createAvailability(elements.get(11), java.time.LocalDateTime.of(2024, 11, 1, 12, 12), java.time.LocalDateTime.of(2024, 12, 1, 12, 12), ChronoUnit.DAYS, 0);
-        availabilityDAO.createAvailability(elements.get(12), java.time.LocalDateTime.of(2024, 12, 31, 12, 12), java.time.LocalDateTime.of(2024, 12, 31, 12, 12), ChronoUnit.DAYS, 0);
-        availabilityDAO.createAvailability(elements.get(13), java.time.LocalDateTime.of(2024, 12, 30, 12, 12), java.time.LocalDateTime.of(2024, 12, 30, 12, 12), ChronoUnit.DAYS, 0);
-        availabilityDAO.createAvailability(elements.get(14), java.time.LocalDateTime.of(2024, 12, 29, 12, 12), java.time.LocalDateTime.of(2024, 12, 29, 12, 12), ChronoUnit.DAYS, 0);
+        availabilityDAO.createAvailability(elements.get(12), java.time.LocalDateTime.of(2024, 10, 31, 12, 12), java.time.LocalDateTime.of(2024, 12, 31, 12, 12), ChronoUnit.DAYS, 0);
+        availabilityDAO.createAvailability(elements.get(13), java.time.LocalDateTime.of(2024, 11, 30, 12, 12), java.time.LocalDateTime.of(2024, 12, 30, 12, 12), ChronoUnit.DAYS, 0);
+        availabilityDAO.createAvailability(elements.get(14), java.time.LocalDateTime.of(2024, 11, 29, 12, 12), java.time.LocalDateTime.of(2024, 12, 29, 12, 12), ChronoUnit.DAYS, 0);
 
     }
 }
